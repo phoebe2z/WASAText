4068bfd0d8fa200ab2aab007883b33322e72b675
Phoebe <149281825+phoebe2z@users.noreply.github.com>
2026-01-07 12:33:31 +0100

    Add files via upload

--- cmd/webapi/main.go
+++ cmd/webapi/main.go
@@ -1,3 +1,26 @@
+/*
+Webapi is the executable for the main web server.
+It builds a web server around APIs from `service/api`.
+Webapi connects to external resources needed (database) and starts two web servers: the API web server, and the debug.
+Everything is served via the API web server, except debug variables (/debug/vars) and profiler infos (pprof).
+
+Usage:
+
+	webapi [flags]
+
+Flags and configurations are handled automatically by the code in `load-configuration.go`.
+
+Return values (exit codes):
+
+	0
+		The program ended successfully (no errors, stopped by signal)
+
+	> 0
+		The program ended due to an error
+
+Note that this program will update the schema of the database to the latest version available (embedded in the
+executable during the build).
+*/
 package main
 
 import (
@@ -5,19 +28,22 @@ import (
 	"database/sql"
 	"errors"
 	"fmt"
+	"math/rand"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+
 	"git.sapienzaapps.it/fantasticcoffee/fantastic-coffee-decaffeinated/service/api"
 	"git.sapienzaapps.it/fantasticcoffee/fantastic-coffee-decaffeinated/service/database"
 	"git.sapienzaapps.it/fantasticcoffee/fantastic-coffee-decaffeinated/service/globaltime"
 	"github.com/ardanlabs/conf"
 	_ "github.com/mattn/go-sqlite3"
 	"github.com/sirupsen/logrus"
-	"math/rand"
-	"net/http"
-	"os"
-	"os/signal"
-	"syscall"
 )
 
+// main is the program entry point. The only purpose of this function is to call run() and set the exit code if there is
+// any error
 func main() {
 	if err := run(); err != nil {
 		_, _ = fmt.Fprintln(os.Stderr, "error: ", err)
@@ -148,8 +174,7 @@ func run() error {
 
 		// Log the status of this shutdown.
 		switch {
-		case sig == syscall.SIGSTOP:
-			return errors.New("integrity issue caused shutdown")
+
 		case err != nil:
 			return fmt.Errorf("could not stop server gracefully: %w", err)
 		}
